d6c95cce3c3900f24cb893b65636970c4ee48bf9
Leonardo Medeiros <Leonardo Medeiros>
2017-01-11 15:48:37 -0300

    aula 38: login e logout
    Signed-off-by: Leonardo Medeiros <Leonardo Medeiros>

--- venv33/code/simplemooc/simplemooc/accounts/urls.py
+++ venv33/code/simplemooc/simplemooc/accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = patterns('',
     	{'template_name': 'accounts/login.html'}, name='login'),    
     url(r'^cadastre-se/$', 'simplemooc.accounts.views.register',
     	name='register'),    
+    url(r'^sair/$', 'django.contrib.auth.views.logout',
+    	{'next_page': 'core:home'}, name='logout'),    
 )
